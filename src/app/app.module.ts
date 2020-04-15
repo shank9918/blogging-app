6f24a7170432ba7bf5ffc4071a35b198468ca685
Shashank Gowri <shashankgowri@gmail.com>
2020-04-15 22:15:32 +0530

    adding forums component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForumsComponent } from './forums/forums.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumsComponent
   ],
   imports: [
     BrowserModule,
